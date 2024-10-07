a9b2fcbdfaab3e74650cec8ef4b34efc3bb5f77f
rdthree <1165540+rdthree@users.noreply.github.com>
2024-10-07 01:04:31 -0400

    mdx is rendering now

--- src/utils/loadPosts.js
+++ src/utils/loadPosts.js
@@ -2,24 +2,17 @@
 import fs from 'fs';
 import path from 'path';
 
-// Function to load all posts
 export function getAllPosts() {
-  // Read the content directory
   const contentDir = path.join(process.cwd(), 'content');
   const postFolders = fs.readdirSync(contentDir);
 
   return postFolders
     .filter((folder) => {
-      // Check if folder contains an MDX file
-      return fs.existsSync(path.join(contentDir, folder, 'sketch.mdx'));
+      const folderPath = path.join(contentDir, folder);
+      const files = fs.readdirSync(folderPath);
+      return files.some(file => path.extname(file).toLowerCase() === '.mdx');
     })
     .map((folder) => {
-      const postFilePath = path.join(contentDir, folder, 'sketch.mdx');
-      const postContent = fs.readFileSync(postFilePath, 'utf8');
-
-      return {
-        folder,
-        content: postContent,
-      };
+      return { folder };
     });
 }
