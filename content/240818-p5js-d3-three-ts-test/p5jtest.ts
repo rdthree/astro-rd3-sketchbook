c56f87afaf28b30d0add217a5acf69f88bcc81a4
rdthree <1165540+rdthree@users.noreply.github.com>
2024-10-16 22:47:05 -0400

    p5 and d3 now work with es6 libraries

--- content/240818-p5js-d3-three-ts-test/p5jtest.ts
+++ content/240818-p5js-d3-three-ts-test/p5jtest.ts
@@ -1,6 +1,6 @@
 // filename: p5jtest.ts
 const p5jtest = (p: typeof p5) => {
-
+  
     // The setup function to initialize the sketch
     p.setup = () => {
         p.createCanvas(1000, 400);
@@ -13,4 +13,4 @@ const p5jtest = (p: typeof p5) => {
     };
 };
 // Create a new p5 instance using sketch
-let p5test = new p5(p5jtest, document.getElementById('p5jtest') as HTMLElement);
+let p5test = new p5(p5jtest, document.getElementById('p5jtest'));
