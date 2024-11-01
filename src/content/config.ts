f3dedcf53abb9818be34b2bbab4d30923a962de3
rdthree <1165540+rdthree@users.noreply.github.com>
2024-10-31 22:01:48 -0400

    trying to preserve original case from folder names in astro routing and links, not working yet

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,14 @@
 // src/content/config.ts
 // TODO: Astro has a problem with this astro:content import in this file, on GitHub already
 // @ts-ignore
-import {defineCollection} from 'astro:content';
+import { defineCollection } from 'astro:content';
 
 export const collections = {
     sketches: defineCollection({
-        type: 'content'
+        type: 'content',
+        slug: ({ id }) => {
+            // Preserve the exact case of the original file path
+            return id.toString();
+        }
     })
 };
