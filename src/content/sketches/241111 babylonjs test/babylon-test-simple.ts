2a7869226a8fe5f50ef7fa6430c797071afd1471
rdthree <1165540+rdthree@users.noreply.github.com>
2024-11-14 21:26:16 -0500

    created astro component instead of the jsx component that was initially used. It consists of a workaround where instead of depending on frontmatter, it uses a ‘renderer’ typescript file and a ‘loader’ astro component.

---
+++ src/content/sketches/241111 babylonjs test/babylon-test-simple.ts
@@ -0,0 +1,8 @@
+﻿import { defineSketch } from '../../../renderers/babylonRenderer';
+
+export default defineSketch(({ scene, Vector3, HemisphericLight, MeshBuilder }) => {
+    // Create light
+    const light = new HemisphericLight('light1', new Vector3(1, 1, 0), scene);
+    // Create sphere
+    const sphere = MeshBuilder.CreateSphere('sphere', { diameter: 1 }, scene);
+});
---
+++ src/renderers/babylonRenderer.ts
@@ -0,0 +1,29 @@
+﻿export async function getBabylon() {
+    const { Engine } = await import('@babylonjs/core/Engines/engine');
+    const { Scene } = await import('@babylonjs/core/scene');
+    const { ArcRotateCamera } = await import('@babylonjs/core/Cameras/arcRotateCamera');
+    const { Vector3 } = await import('@babylonjs/core/Maths/math.vector');
+    const { HemisphericLight } = await import('@babylonjs/core/Lights/hemisphericLight');
+    const { MeshBuilder } = await import('@babylonjs/core/Meshes/meshBuilder');
+    const { StandardMaterial } = await import('@babylonjs/core/Materials/standardMaterial');
+
+    return {
+        Engine,
+        Scene,
+        ArcRotateCamera,
+        Vector3,
+        HemisphericLight,
+        MeshBuilder,
+        StandardMaterial,
+    };
+}
+
+export type Babylon = Awaited<ReturnType<typeof getBabylon>>;
+export type SketchInitializer = (
+    context: Omit<Babylon, 'Engine' | 'Scene'> & { scene: InstanceType<Babylon['Scene']> }
+) => void | Promise<void>;
+export type DefinedSketch = ReturnType<typeof defineSketch>;
+
+export function defineSketch(initSketch: SketchInitializer) {
+    return initSketch
+}
