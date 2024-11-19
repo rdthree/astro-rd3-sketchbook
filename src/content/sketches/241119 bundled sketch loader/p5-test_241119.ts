77a9b4f732af44d61642cac4fbf60c73d1213f8a
rdthree <1165540+rdthree@users.noreply.github.com>
2024-11-18 23:36:47 -0500

    almost have p5 working as a bundled module

---
+++ src/content/sketches/241119 bundled sketch loader/p5-test_241119.ts
@@ -0,0 +1,23 @@
+﻿import { defineSketch } from '../../../renderers/p5Renderer';
+
+export default defineSketch(({ sketch }) => {
+    sketch.setup = () => {
+        const canvas = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
+        // Get the canvas wrapper div element - p5 types are satisfied
+        const wrapper = document.querySelector('.p5-sketch');
+        if (wrapper) {
+            canvas.parent(wrapper);
+        }
+    };
+
+    sketch.draw = () => {
+        sketch.background(0);
+        sketch.fill(255, 0, 0);
+        sketch.noStroke();
+        sketch.ellipse(sketch.width/2, sketch.height/2, 100, 100);
+    };
+
+    sketch.windowResized = () => {
+        sketch.resizeCanvas(sketch.windowWidth, sketch.windowHeight);
+    };
+});
---
+++ src/renderers/p5Renderer.ts
@@ -0,0 +1,18 @@
+﻿import type p5 from 'p5';
+
+export async function getP5() {
+    const p5Module = await import('p5');
+    return { p5: p5Module.default };
+}
+
+export async function createScene(canvas: HTMLCanvasElement) {
+    const { p5 } = await getP5();
+    let p5Instance = new p5(() => {}, canvas);
+    return { sketch: p5Instance };
+}
+
+export type SketchInitializer = (context: { sketch: p5 }) => void | Promise<void>;
+
+export function defineSketch(initSketch: SketchInitializer) {
+    return initSketch;
+}
