2a7ee0c65ff68a00852a42e8df416ae40ba1974e
rdthree <1165540+rdthree@users.noreply.github.com>
2024-11-26 22:34:14 -0500

    animation portion of threejs-journey

--- src/content/sketches/241126 threejs journey/threejs-journey-firstproject_241126.ts
+++ src/content/sketches/241126 threejs journey/threejs-journey-firstproject_241126.ts
@@ -4,10 +4,20 @@ import * as THREE from 'three';
 export default defineSketch(({ scene, renderer }) => {
     
     // scene already included in renderer import
+    scene.background = new THREE.Color('ghostwhite');
+    
+    // soft lighting
+    const ambientLight = new THREE.AmbientLight('skyblue', 0.5);
+    scene.add(ambientLight);
+    
+    // directional light
+    const directionalLight = new THREE.DirectionalLight('orange', 2.5);
+    directionalLight.position.set(1, 1, 1);
+    scene.add(directionalLight);
     
     // axes helper
     const axesHelper = new THREE.AxesHelper(1.2);
-    scene.add(axesHelper);
+    //scene.add(axesHelper);
     
     // sizes
     const sizes = { width: 800, height: 600 };
@@ -16,35 +26,36 @@ export default defineSketch(({ scene, renderer }) => {
     const boxGroup = new THREE.Group();
     boxGroup.scale.set(1.0, 2.0, 1.0);
     boxGroup.rotation.set(0.0, 0.2, 0.0);
+    
+    directionalLight.lookAt(boxGroup.position);
     scene.add(boxGroup);
 
     // objects
     const cube1 = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshBasicMaterial({ color: 'skyblue' })
+        new THREE.MeshStandardMaterial({ color: 'ghostwhite' })
     );
     cube1.position.set(-2.0, 0.0, 0.0);
     boxGroup.add(cube1);
     
     const cube2 = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshBasicMaterial({ color: 'skyblue' })
+        new THREE.MeshStandardMaterial({ color: 'ghostwhite' })
     );
     cube2.position.set(0.0, 0.0, 0.0);
     boxGroup.add(cube2);
     
     const cube3 = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshBasicMaterial({ color: 'skyblue' })
+        new THREE.MeshStandardMaterial({ color: 'ghostwhite' })
     );
     cube3.position.set(2.0, 0.0, 0.0);
     boxGroup.add(cube3);
-
-
+    
 
     // camera
     const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-    camera.position.z = 3.5; 
+    camera.position.z = 5.5; 
     scene.add(camera);
     
     // mess with the camera
@@ -52,6 +63,36 @@ export default defineSketch(({ scene, renderer }) => {
     
     // render is already included in renderer import
     renderer.setSize(sizes.width, sizes.height);
-    renderer.render(scene, camera);
- 
+    
+    // animation
+    const clock = new THREE.Clock();
+    const tick = () =>
+    {
+        console.log('tick');
+        // time
+        const elapsedTime = clock.getElapsedTime();
+        
+        // update objects
+        boxGroup.rotation.y = 0.65 * elapsedTime;
+        directionalLight.rotation.y = 2.5 * elapsedTime;
+        cube1.position.y = Math.sin(elapsedTime);
+        cube2.position.y = Math.cos(elapsedTime)
+        cube3.position.z = Math.tan(elapsedTime);
+
+        cube1.scale.x = 1 + Math.cos(elapsedTime * 0.8) * 0.5;
+        cube2.scale.z = 1 + Math.sin(elapsedTime * 0.8) * 0.5;
+        cube3.scale.y = 1 + Math.sin(elapsedTime * 0.8) * 0.5;
+
+
+
+        // render
+        renderer.render(scene, camera);
+
+        // call tick again on the next frame
+        window.requestAnimationFrame(tick);
+    }
+
+    tick();
+
+
 });
